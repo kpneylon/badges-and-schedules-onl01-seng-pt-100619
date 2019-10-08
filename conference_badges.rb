08bfd3a625801f66aa3c560ec28fe632ffae0ce8
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-10-08 20:37:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  puts "Hello, my name is #{name}"
 end  
 
 def batch_badge_creator(array)
